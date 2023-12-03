32095d5438fb02fe2c753d4f6deac0451c574f06
Patryk Okenczyc <Xomel@outlook.com>
2023-12-03 01:45:39 +0100

    Added Product and Category also Created Database

---
+++ ECommerce.Database/CBContext.cs
@@ -0,0 +1,20 @@
+﻿using ECommerce.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Database
+{
+    public class CBContext : DbContext
+    {
+        public CBContext() : base("ECommerceConnection") 
+        { 
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ECommerce.Database/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace ECommerce.Database.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ECommerce.Database.CBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ECommerce.Database.CBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
